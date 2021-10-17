40c863400f6411dfe7c10c94d4852824ac52acf5
francosbenitez <francosbenitez@gmail.com>
2021-10-17 20:09:49 -0300

    fix: fixed problem with getting all bookmarks

--- src/components/SongsBookmarks.vue
+++ src/components/SongsBookmarks.vue
@@ -1,20 +1,15 @@
 <template>
     <Panel title="Bookmarks">
         <div>
-            <b-table striped hover :items="bookmarks">
-                <td>
-                    Algo
-                </td>
-                <td>
-                    Algo otro
-                </td>
-            </b-table>
+           <b-table striped hover :items="bookmarks"></b-table>
         </div>
     </Panel>
 </template>
 
 <script>
 import Panel from '@/components/Panel'
+import {mapState} from 'vuex'
+import BookmarksService from '@/services/BookmarksService'
 
 export default {
   data () {
@@ -24,6 +19,19 @@ export default {
   },
   components: {
     Panel
+  },
+  computed: {
+    ...mapState([
+      'isUserLoggedIn'
+      // 'user'
+    ])
+  },
+  async mounted () {
+    if (this.isUserLoggedIn) {
+      this.bookmarks = (await BookmarksService.index({
+        userId: this.$store.state.user.id
+      })).data
+    }
   }
 }
 </script>
